4d5871628a6d669a8e5a8e7be4fce8da6df1a484
KindlyDrow <116948580+KindlyDrow@users.noreply.github.com>
2023-06-02 21:53:04 +0300

    FixUI

--- Assets/Scripts/GameUI/UIManager.cs
+++ Assets/Scripts/GameUI/UIManager.cs
@@ -4,26 +4,19 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject m_conversionDataUI;
     [SerializeField] private GameObject m_preGameUI;
     [SerializeField] private GameObject m_inGameUI;
     [SerializeField] private GameObject m_endGameUI;
 
     [SerializeField] private Button m_startGameButton;
-    [SerializeField] private Button m_okButton;
     [SerializeField] private Button m_restartGameButton;
 
     private void Start()
     {
         HideAll();
-        m_conversionDataUI.SetActive(true); 
+        m_preGameUI.SetActive(true); 
         m_startGameButton.onClick.AddListener(StartGame);
         m_restartGameButton.onClick.AddListener(ReStartGame);
-        m_okButton.onClick.AddListener(() =>
-        {
-            HideAll();
-            m_preGameUI.SetActive(true);
-        });
         GameManager.Instance.OnPreGameStarted += GameManager_OnPreGameStarted;
         GameManager.Instance.OnInGameStarted += GameManager_OnInGameStarted;
         GameManager.Instance.OnEndGameStarted += GameManager_OnEndGameStarted;
@@ -59,7 +52,6 @@ public class UIManager : MonoBehaviour
 
     private void HideAll()
     {
-        m_conversionDataUI.SetActive(false);
         m_preGameUI.SetActive(false);
         m_inGameUI.SetActive(false);
         m_endGameUI.SetActive(false);
